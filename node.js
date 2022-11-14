a585956005253832e073a8744cb8366bcab51819
arzymva <115873282+arzymva@users.noreply.github.com>
2022-11-14 18:33:02 +0400

    Add files via upload

---
+++ node.js
@@ -0,0 +1,26 @@
+class Message{
+    arr=[];
+    constructor(name,text){
+       this.name=name;
+       text=this.text
+    }
+        toString(){
+            return str=this.name+this.text+this.date;
+        }
+        
+    }
+    class Messenger extends Message{
+        show_history(){
+            let now = this.date;
+          this.arr.forEach((item)=>{
+            console.log(`${item.date.getHours()}:${item.date.getMinutes()}:${item.name}:${item.texter}}`)
+          })
+        }
+        send(author, text){
+           this.arr.push({name:author,texter:text,date:new Date()})
+        }
+    }
+    let messenger = new Messenger()
+    messenger.send('Arzushka', 'hi')
+    messenger.send('Nilichka', 'hi :)')
+    messenger.show_history()
